9aea5b8d8beb678a13cd1c1bd07b8f60b5e9575c
Irene De Nicolo <irene.denicolo89@gmail.com>
2023-03-04 19:39:08 +0100

    basic logic working when uploading any audio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,70 @@
+import React, { useState } from 'react';
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+type Props = {
+  whisperEndpoint: string;
+  authToken: string;
+}
+
+
+export default function Home({ whisperEndpoint, authToken } : Props) {
+  const [file, setFile] = useState(null);
+  const [result, setResult] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const uploadFile = async (file: any) => {
+    console.log('FILE1', file)
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("model", "whisper-1");
+    console.log('formData', formData)
+    console.log('authToken', authToken)
+
+    const data = await fetch(whisperEndpoint, {
+      headers:{
+        "Authorization": `Bearer ${authToken}`,
+      },
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        console.log('response!!', response)
+        return response.json()
+      })
+      .then((result) => {
+        console.log('result!!', result)
+        return result;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    return data;
+  };
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    const res = await uploadFile(file);
+    console.log('res in index', res)
+    setResult(res.text);
+    setLoading(false);
+  };
+
+
+  const handleChange = (e:any) => {
+    console.log("EVENT!", e.target)
+    if (e.target.files && e.target.files[0]) {
+      setFile(e.target.files[0])
+      const body = new FormData();
+      body.append("image", e.target.files[0]);
+    }
+  }
+
   return (
     <>
       <Head>
@@ -15,109 +74,41 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+        <form onSubmit={handleSubmit}>
+          <input
+            type="file"
+            accept="audio/*"
+            name="file"
+            onChange={handleChange}
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+          {/* <input
+            type="text"
+            name="youtube"
+            onChange={handleChange}
+          /> */}
+        <button type="submit">Process</button>
+      </form>
+      <div className="results">
+        {result && !loading && (
+          <div>
+            <h3>Transcription</h3>
+            {result}
           </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        )}
+      </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps = () => {
+  const whisperEndpoint = process.env.WHISPER_ENDPOINT
+  const authToken = process.env.AUTHORIZATION_TOKEN
+
+  return {
+    props: { 
+      whisperEndpoint,
+      authToken
+    },
+  }
 }
